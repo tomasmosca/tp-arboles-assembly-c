f776bb1a5ca39848a67ed89857485a78dd64cb68
tomasmosca <tomasmoscarelli@hotmail.com>
2022-09-25 15:11:02 -0300

    creado main.c con definicion de funciones

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arbol_binario.h>
+
+struct arbol_binario crearArbolB(int valor, arbol_binario *izq, arbol_binario *der);
+struct arbol_binario buscarMin(arbol_binario *arbol);
+void eliminarTodos(arbol_binario *arbol, int valor);
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
